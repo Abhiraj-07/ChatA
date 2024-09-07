fef7a6548b5bfe801caebe6ebb0cac462147a2e5
Abhiraj Trivedi <trivediabhiraj7@gmail.com>
2024-09-08 02:01:18 +0530

    deployed

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,26 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";
 import { server, app } from "./socket/socket.js";
 
+import path from "path";
+
+const __dirname = path.resolve();
+
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 app.use(express.json());
 app.use(cookieParser());
 // root
-app.get("/", (req, res) => {
-  res.send("hello sever");
-});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(port, () => {
   connectToMongoDB();
   console.log(`server started ON PORT ${port}`);
